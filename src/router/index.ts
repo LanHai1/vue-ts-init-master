ed88d0ec8ed477f28479c8598823dc46ee924a0b
兰海 <lan.hai@z2a.com.cn>
2020-03-07 22:27:47 +0800

    初始化项目 ts 动态路由 bpmn rem iconfont 等

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,26 +4,41 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
+const constantRouterMap = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: Home,
+    redirect: "/userAction/login"
+  }, { // 用户登陆注册操作
+    path: '/userAction',
+    name: 'userAction',
+    component: () => import(/* webpackChunkName: "loginAction" */ '@/views/userAction/Login.vue'),
+    children: [{
+      path: 'login',
+      components: {
+        loginview: () => import(/* webpackChunkName: "LoginFill" */ "@/components/login/LoginFill.vue")
+      }
+    }, {
+      path: 'register',
+      components: {
+        loginview: () => import(/* webpackChunkName: "RegisterFill" */ "@/components/register/RegisterFill.vue")
+      }
+    }]
+  }, {
+    path: "/main",
+    name: "main",
+    component: () => import(/* webpackChunkName: "layout" */ '@/views/layout/index.vue')
+  }, {
+    path: "/404",
+    name: "notFound",
+    component: () => import(/* webpackChunkName: "notFound" */ '@/views/notFound/index.vue')
   }
 ]
 
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
 
-export default router
+export default new VueRouter({
+  mode: 'hash',
+  base: process.env.BASE_URL,
+  routes: constantRouterMap
+})
